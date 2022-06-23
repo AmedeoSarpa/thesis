e6b74f52255722e00e6048cb5fe78a5e72e0c2a2
Amedeo SARPA <amedeo.sarpa@alten.com>
2022-06-23 18:28:27 +0200

    update occurences number

--- correlation-coefficient/correlation-coefficient.py
+++ correlation-coefficient/correlation-coefficient.py
@@ -2,43 +2,48 @@ from cmath import cos, pi
 from math import sqrt
 
 
-def correlationComputation(matrix,name) :
-        a = matrix[0][0]
-        b = matrix[0][1]
-        c = matrix[1][0]
-        d = matrix[1][1]
+def correlationComputation(matrix, name):
+    a = matrix[0][0]
+    b = matrix[0][1]
+    c = matrix[1][0]
+    d = matrix[1][1]
+    try:
         den = sqrt((((a*d)/b)/c))
         val = pi/(1+den)
         coeff = cos(val)
         print(f"{name} correlation coefficient : {coeff:.3f}")
+    except:
+        print(f"Is not possible to compute the correlation for {name}")
 
-if __name__ == "__main__" :
-        
-        try:
-            fp = open('story-list.csv','r')
-        except :
-            print("Error opening file")
-            
-        lines = fp.readlines()[1:]
-        matrix_spike = [[0,0],[0,0]]
-        matrix_new_func = [[0,0],[0,0]]
-        matrix_new_tech = [[0,0],[0,0]]
-        for line in lines :
-            numbers = line.split(',')
-            bad_estimation = int(numbers[6])
-            spike = int(numbers[3])
-            new_tech = int(numbers[4])
-            new_func = int(numbers[5])
-            matrix_spike[bad_estimation][spike] = matrix_spike[bad_estimation][spike] + 1
-            matrix_new_tech[bad_estimation][new_tech]=matrix_new_tech[bad_estimation][new_tech]+1
-            matrix_new_func[bad_estimation][new_func]=matrix_new_func[bad_estimation][new_func]+1
-        
-        i = 0
-        while i < 3 :
-            if i == 0:
-                correlationComputation(matrix_new_func,'New functionalities')
-            elif i == 1:
-                correlationComputation(matrix_new_tech,'New technology')
-            else :
-                correlationComputation(matrix_spike,'Spike')
-            i = i+1
+
+if __name__ == "__main__":
+
+    try:
+        fp = open('story-list.csv', 'r')
+    except:
+        print("Error opening file")
+        exit(1)
+
+    lines = fp.readlines()[1:]
+    matrix_spike = [[0, 0], [0, 0]]
+    matrix_new_func = [[0, 0], [0, 0]]
+    matrix_new_tech = [[0, 0], [0, 0]]
+    for line in lines:
+        numbers = line.split(',')
+        bad_estimation = int(numbers[6])
+        spike = int(numbers[3])
+        new_tech = int(numbers[4])
+        new_func = int(numbers[5])
+        matrix_spike[bad_estimation][spike] = matrix_spike[bad_estimation][spike] + 1
+        matrix_new_tech[bad_estimation][new_tech] = matrix_new_tech[bad_estimation][new_tech]+1
+        matrix_new_func[bad_estimation][new_func] = matrix_new_func[bad_estimation][new_func]+1
+
+    i = 0
+    while i < 3:
+        if i == 0:
+            correlationComputation(matrix_new_func, 'New functionalities')
+        elif i == 1:
+            correlationComputation(matrix_new_tech, 'New technology')
+        else:
+            correlationComputation(matrix_spike, 'Spike')
+        i = i+1
